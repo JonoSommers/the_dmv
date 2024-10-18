96980e8ef2a4f421db624a476e5912e670bdd23a
Jono Sommers <jonosommers95@gmail.com>
2024-10-18 18:12:28 -0500

    register_vehicle method working and tests passing

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,13 +1,13 @@
 class Facility
-  attr_reader :name, :address, :phone, :services, :collected_fees, :registered_vehicles
+  attr_reader :name, :address, :phone, :services, :collected_fees, :registered_vehicles, :today
   def initialize(facility_info)
     @name = facility_info[:name]
     @address = facility_info[:address]
     @phone = facility_info[:phone]
     @services = []
-    @registration_date = nil
     @registered_vehicles = []
     @collected_fees = 0
+    @today = Date.today
   end
 
   def add_service(service)
@@ -15,13 +15,17 @@ class Facility
   end
 
   def register_vehicle(vehicle)
+    vehicle.set_registration_date
     if vehicle.antique? == true
       @collected_fees += 25
+      vehicle.set_plate_type
     elsif 
       vehicle.electric_vehicle? == true
       @collected_fees +=  200
+      vehicle.set_plate_type
     else
       @collected_fees += 100
+      vehicle.set_plate_type
     end
     @registered_vehicles << vehicle
   end
