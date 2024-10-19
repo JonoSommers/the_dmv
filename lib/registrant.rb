cde5d3f82ca52b2e8606fab142429f403bbe8254
Jono Sommers <jonosommers95@gmail.com>
2024-10-19 13:52:48 -0500

    Added tae_written_test method

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -16,6 +16,11 @@ class Registrant
         @permit = true
     end
 
-
-
+    def take_written_test
+        if @age >= 16 && @permit == true
+            @license_data[:written] = true
+        else
+            @license_data[:written] = false
+        end
+    end
 end
