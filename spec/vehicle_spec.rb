dc1244bba0e700ad89f1c989d698f2a72896cbfe
Jono Sommers <jonosommers95@gmail.com>
2024-10-18 19:07:02 -0500

    Made test to see if set_registration_date method was working properly

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Vehicle do
     @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
     @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
     @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+    @today = Date.today
   end
   describe '#initialize' do
     it 'can initialize' do
@@ -15,6 +16,7 @@ RSpec.describe Vehicle do
       expect(@cruz.model).to eq('Cruz')
       expect(@cruz.engine).to eq(:ice)
       expect(@cruz.registration_date).to eq(nil)
+      expect(@today).to eq(Date.today)
     end
   end
 
@@ -31,6 +33,14 @@ RSpec.describe Vehicle do
       expect(@cruz.electric_vehicle?).to eq(false)
       expect(@bolt.electric_vehicle?).to eq(true)
       expect(@camaro.electric_vehicle?).to eq(false)
+    end
+  end
+
+  describe '#set_registration_date' do
+    it 'sets the registration date to the current day when called on a vehicle' do
+      expect(@cruz.registration_date).to be(nil)
+      @cruz.set_registration_date
+      expect(@cruz.registration_date).to eq(@today)
     end
   end
 end
