76f8152e4eb5c623d1a7ef22fb4f10ad8769a274
Jono Sommers <jonosommers95@gmail.com>
2024-10-20 11:27:45 -0500

    Vehicles are being created correctly

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,13 +1,14 @@
 class VehicleFactory
+    attr_reader :vehicles_made
 
     def initialize
-        @created_vehicles = []
+        @vehicles_made = []
     end
 
-    def create_vehilces(wa_ev_registrations)
+    def create_vehicles(wa_ev_registrations)
         wa_ev_registrations.each do |vehicle|
             new_vehicle = Vehicle.new(vehicle)
-            @created_vehicles << new_vehicle
+            @vehicles_made << new_vehicle
         end
     end
 end
